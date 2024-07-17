1f9bfc37e63071bdd80be935c2bfe9260acf23bb
ProgramNS <blise01@hotmail.com>
2024-07-17 22:19:49 +0700

    UPDATE NEW INPUT DATA FOR REVISION SKRIPSI

--- freegul_version_1/app/src/main/java/com/example/freegul_version_1/NewPatientActivity.kt
+++ freegul_version_1/app/src/main/java/com/example/freegul_version_1/NewPatientActivity.kt
@@ -9,6 +9,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import com.example.freegul_version_1.databinding.ActivityInputDataBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
+import java.util.TimeZone
 
 class NewPatientActivity : AppCompatActivity() {
     private lateinit var binding: ActivityInputDataBinding
@@ -26,19 +31,12 @@ class NewPatientActivity : AppCompatActivity() {
 
         binding.kirimData.setOnClickListener {
             if (isValidInput()) {
-                val intent = Intent(this, MonitoringActivity::class.java)
-                intent.putExtra("nama_lengkap", binding.namaLengkap.text.toString())
-                intent.putExtra("usia", binding.usia.text.toString().toInt())
-                intent.putExtra("No_hp", binding.hp.text.toString())
-                intent.putExtra("jenis_kelamin", when {
-                    binding.laki.isChecked -> "Laki-Laki"
-                    binding.perempuan.isChecked -> "Perempuan"
-                    else -> ""
-                })
-                startActivity(intent)
+                saveDataToDatabase()
+//                val intent = Intent(this, MonitoringActivity::class.java)
+//                startActivity(intent)
                 Toast.makeText(this , "Silahkan Lanjutkan Pengecekan Gula Darah", Toast.LENGTH_SHORT).show()
             } else {
-                Toast.makeText(this, "Data tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Tidak Bisa Lanjut Ada Data Yang Kosong", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -67,6 +65,7 @@ class NewPatientActivity : AppCompatActivity() {
     }
 
     private fun validateInput() {
+        val nik = binding.nik.text.toString()
         val namaLengkap = binding.namaLengkap.text.toString()
         val usia = binding.usia.text.toString()
         val hp = binding.hp.text.toString()
@@ -75,7 +74,7 @@ class NewPatientActivity : AppCompatActivity() {
             binding.perempuan.isChecked -> "Perempuan"
             else -> ""
         }
-
+        binding.nik.error = if (nik.isEmpty()) "NIK tidak boleh kosong!" else null
         binding.namaLengkap.error = if (namaLengkap.isEmpty()) "Nama lengkap tidak boleh kosong!" else null
         binding.usia.error = if (usia.isEmpty()) "Usia tidak boleh kosong!" else null
         binding.hp.error = if (hp.isEmpty()) "No handphone tidak boleh kosong!" else null
@@ -91,6 +90,7 @@ class NewPatientActivity : AppCompatActivity() {
     }
 
     private fun isValidInput(): Boolean {
+        val nik = binding.nik.text.toString()
         val namaLengkap = binding.namaLengkap.text.toString()
         val usia = binding.usia.text.toString()
         val hp = binding.hp.text.toString()
@@ -100,9 +100,43 @@ class NewPatientActivity : AppCompatActivity() {
             else -> ""
         }
 
-        return namaLengkap.isNotEmpty() && usia.isNotEmpty() && hp.isNotEmpty() && jenisKelamin.isNotEmpty()
+        return nik.isNotEmpty() && namaLengkap.isNotEmpty() && usia.isNotEmpty() && hp.isNotEmpty() && jenisKelamin.isNotEmpty()
     }
+    private fun saveDataToDatabase() {
+        val nik = binding.nik.text.toString()
+        val namaLengkap = binding.namaLengkap.text.toString()
+        val usia = binding.usia.text.toString().toInt()
+        val hp = binding.hp.text.toString()
+        val jenisKelamin = when {
+            binding.laki.isChecked -> "Laki-Laki"
+            binding.perempuan.isChecked -> "Perempuan"
+            else -> ""
+        }
 
+        val userId = auth.currentUser?.uid ?: return
+        val database = FirebaseDatabase.getInstance("https://skripsi-a9be0-default-rtdb.asia-southeast1.firebasedatabase.app/")
+        val dataRef = database.getReference("users").child(userId).child("pasien").child(nik)
+        val currentDateTime = Calendar.getInstance().time
+        val dateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm:ss", Locale.getDefault())
+        dateFormat.timeZone = TimeZone.getTimeZone("Asia/Jakarta")
+        val formattedDate = dateFormat.format(currentDateTime)
+
+        val data = mapOf(
+            "Nama_Lengkap" to namaLengkap,
+            "Usia" to usia,
+            "Jenis_Kelamin" to jenisKelamin,
+            "No_Handphone" to hp,
+            "Tanggal_Pembuatan" to formattedDate
+        )
+
+        dataRef.setValue(data).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "Data berhasil disimpan", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Gagal menyimpan data", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
 
 
--- freegul_version_1/app/src/main/java/com/example/freegul_version_1/Patient.kt
+++ freegul_version_1/app/src/main/java/com/example/freegul_version_1/Patient.kt
@@ -1,7 +1,7 @@
 package com.example.freegul_version_1
 
 data class Patient(
-    val key: String = "",
+    val nik: String = "",
     val jenis_kelamin: String = "",
     val nama_lengkap: String = "",
     val nilai_gula_darah: Double = 0.0,
